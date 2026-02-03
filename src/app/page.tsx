23b7a7e0ea876899ee0803671c1f50752e26e2bd
Rene Marves <rfmarves@gmail.com>
2026-02-03 17:43:11 -0600

    completed footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import styles from "./page.module.css";
 import HandcraftHeader from "./components/handcraft-header";
+import HandcraftFooter from "./components/handcraft-footer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <HandcraftHeader />
         <div className={styles.intro}>
-          <HandcraftHeader />
+          <h2>Welcome to Handcraft Haven!</h2>
+          <p>Your one-stop shop for unique, handmade crafts.</p>
         </div>
+        <div className={styles.content}>
+        </div>
+      <HandcraftFooter />
       </main>
     </div>
   );
