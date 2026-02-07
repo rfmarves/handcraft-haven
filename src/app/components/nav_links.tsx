62936228dafae5d2c7cd12a2f83d1f1b517932f0
rfmarves <rfmarves@gmail.com>
2026-02-07 11:22:23 -0600

    Replace icons with react-icons and update nav styles

--- src/app/components/nav_links.tsx
+++ src/app/components/nav_links.tsx
@@ -2,41 +2,45 @@
 
 /**import home, user-circle, tag, user */
 import {
-  HomeIcon,
-  TagIcon,
-  UserGroupIcon,
-  UserIcon,
-} from "@heroicons/react/24/solid"
+  SlHome,
+  SlTag,
+  SlUser,
+  SlLogin,
+  SlBasket
+} from "react-icons/sl";
 
 /**navigations uses the Link library and usePathname */
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import CartButton from "./cart-button"
 import styles from "../page.module.css"
-import clsx from "clsx"
 
 /**links arrays */
 const links = [
   {
     name: "Home",
     href: "/",
-    icon: HomeIcon,
+    icon: SlHome,
   },
   {
     name: "Sellers Profile",
     href: "/sellers",
-    icon: UserGroupIcon,
+    icon: SlUser,
   },
   {
     name: "Product Listings",
     href: "/dashboard/products",
-    icon: TagIcon,
+    icon: SlTag,
   },
   {
     name: "Login",
     href: "/login",
-    icon: UserIcon,
+    icon: SlLogin,
   },
+  {
+    name: "Cart",
+    href: "/cart",
+    icon: SlBasket,
+  }
 ]
 
 /**export the link function. use map to assigne the links to the respective areas */
@@ -44,35 +48,19 @@ export default function Navlinks() {
   const pathname = usePathname()
 
   return (
-    <nav
-      className={clsx(
-        "flex items-center gap-3 h-[56px] px-4",
-        "bg-white text-black-900", // 👈 lock text color
-        "border-b shadow-sm",
-        styles.nav,
-      )}
-    >
+    <nav className={styles.navBlock} >
       {links.map((link) => {
         const LinkIcon = link.icon
         return (
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(
-              "text-black-900 flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              styles.headerlinks,
-              {
-                "bg-sky-100 text-blue-600": pathname === link.href,
-              },
-            )}
+            className={styles.navLink}
           >
             <LinkIcon className={styles.headericons} />
-            {/* <p className="hidden md:block">{link.name}</p> */}
           </Link>
         )
       })}
-      {/**add the cartButon here */}
-      <CartButton />
     </nav>
   )
 }
