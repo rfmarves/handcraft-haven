06132ff316bd3b03f6b1ef39fb1b14a93c0e84ea
rfmarves <rfmarves@gmail.com>
2026-02-07 11:26:58 -0600

    Refactor HandcraftFooter component structure

--- src/app/components/handcraft-footer.tsx
+++ src/app/components/handcraft-footer.tsx
@@ -1,27 +1,19 @@
-import styles from "../page.module.css"
-import { FaFacebook, FaInstagram } from "react-icons/fa"
+import styles from "../page.module.css";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 export default function HandcraftFooter() {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.footerText}>
-        <p>© 2024 Handcraft Haven.</p>
-        <p>Crafted with ❤️ by Handcraft Haven Team.</p>
-      </div>
-      <div className={styles.socialIcons}>
-        <a
-          href="https://www.facebook.com/handcrafthaven"
-          className={styles.footerLink}
-        >
-          <FaFacebook size={20} />
-        </a>
-        <a
-          href="https://www.instagram.com/handcrafthaven"
-          className={styles.footerLink}
-        >
-          <FaInstagram size={20} />
-        </a>
-      </div>
-    </footer>
-  )
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footerText}>
+                <p>© 2024 Handcraft Haven.</p>
+                <p>
+                    Crafted with ❤️ by Handcraft Haven Team.
+                </p>
+            </div>
+            <div className={styles.socialIcons}>
+                <a href="https://www.facebook.com/handcrafthaven" className={styles.footerLink}><FaFacebook size={20} /></a>
+                <a href="https://www.instagram.com/handcrafthaven" className={styles.footerLink}><FaInstagram size={20} /></a>
+            </div>
+        </footer>
+    )
 }
