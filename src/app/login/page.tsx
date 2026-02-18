66b8fb776500482babe4fd28b23cdc05e6a761a4
Rene Marves <rfmarves@gmail.com>
2026-02-17 19:39:17 -0600

    designed login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <h1>Login page</h1>;
+import styles from "./page.module.css";
+import LoginForm from './login-form';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata = {
+  title: 'Login',
+};
+
+export default function LoginPage() {
+  return (
+    <div className={styles.loginContainer}>
+     <div className={styles.formHeader}><h1>Handcraft Haven</h1></div>
+     <div className={styles.formContainer}>
+      <Suspense>
+        <LoginForm />
+      </Suspense>
+     </div>
+    </div>
+    );
 }
